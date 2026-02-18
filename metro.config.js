29a49765dcb799f96dd33475da47275949741295
gershinho <kpatel6607@gmail.com>
2026-02-18 11:46:34 -0600

    added eshaan updates

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,15 @@ const config = getDefaultConfig(__dirname);
 // Add .db to asset extensions so the SQLite database gets bundled
 config.resolver.assetExts.push('db');
 
+// Configure SVG transformer
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
 module.exports = config;
